26819e600557331648db083579645050f0f8d26a
Silberh <svenja.lutz@live.de>
2022-02-21 17:15:20 +0100

    js eingefügt

---
+++ eigene_website/code.js
@@ -0,0 +1,9 @@
+var options = [
+    'this text was inserted through a click',
+    'option two',
+    'option three'
+];
+
+function option1(){
+    document.getElementById('doThis').innerHTML = options[0];
+}
