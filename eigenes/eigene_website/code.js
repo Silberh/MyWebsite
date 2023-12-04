1d6b407d0bc5b0c74e38e2aaaaa0d131553f33e9
Silberh <svenja.lutz@live.de>
2023-12-04 15:24:22 +0100

    clean up
    clean up of old things to local

--- eigenes/eigene_website/code.js
+++
@@ -1,9 +0,0 @@
-var options = [
-    'this text was inserted through a click',
-    'option two',
-    'option three'
-];
-
-function option1(){
-    document.getElementById('doThis').innerHTML = options[0];
-}
