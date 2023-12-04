30a3a6d68c846fbab3b98c762b96144743d9786a
Silberh <svenja.lutz@live.de>
2023-12-04 15:17:36 +0100

    Preparation emailjs
    Javascript insert
    File references created

---
+++ plainly/index.js
@@ -0,0 +1,24 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    }
+}
+
+const serviceID = "enterserviceid";
+const templateID ="entertemplateid";
+
+emailjs
+.send(serviceID, templateID, params)
+.then((res) => {
+    document.getElementById("name").value ="";
+    email.getElementById("email").value ="";
+    subject.getElementById("subject").value ="";
+    message.getElementById("message").value ="";
+    console.log(res);
+    alert("Your message sent successfully");
+})
+
+.catch((err) => console.log(err));
