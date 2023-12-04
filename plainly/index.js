c512878f1344e27b66ca8212b3b6916de0b54501
Silberh <svenja.lutz@live.de>
2023-12-04 20:20:41 +0100

    small fixes

--- plainly/index.js
+++ plainly/index.js
@@ -4,21 +4,22 @@ function sendMail() {
         email: document.getElementById("email").value,
         subject: document.getElementById("subject").value,
         message: document.getElementById("message").value,
-    }
+    };
+
+    const serviceID = "service_i67j1hf";
+    const templateID = "template_u7ixlz9";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            email.getElementById("email").value = "";
+            subject.getElementById("subject").value = "";
+            message.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully");
+        })
+
+        .catch((err) => console.log(err));
+
 }
-
-const serviceID = "service_i67j1hf";
-const templateID ="template_u7ixlz9";
-
-emailjs
-.send(serviceID, templateID, params)
-.then((res) => {
-    document.getElementById("name").value ="";
-    email.getElementById("email").value ="";
-    subject.getElementById("subject").value ="";
-    message.getElementById("message").value ="";
-    console.log(res);
-    alert("Your message sent successfully");
-})
-
-.catch((err) => console.log(err));
